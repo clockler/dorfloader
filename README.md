592ec9ad8c5a3c83270f428578183379dafa093b
the clockler <hello@clockler.com>
2015-08-03 01:51:44 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dorfloader
+
+Dorfloader is a simple JS resource loader with the least possible implementation overhead for you, the developer.
+
+## Usage
+
+    <script src="res/dorf.min.js" data-manifest="res/manifest.json"></script>
+
+That's really all there is to it! Dorfloader will now load all files listed in `res/manifest.json`, emitting `progress` events as often as every 100ms during the load and a `load` event when all items are complete. Scripts will not be processed until all items are loaded, so feel free to assume that on execution everything is ready for you.
+
+To actually **access your loaded resources**, should you want to, you can use `Dorf.get(resource)` where `resource` is any file as written in your manifest. You can also probably rely on the cache, but I wouldn't know anything about that. I'm just a Javascript monkey.
+
+## Notes
+
+* If omitted, the `data-manifest` attribute is assumed to be simply `manifest.json` relative to the current page.
+* All paths in `manifest.json` are relevant to said file.
+* As of this writing, Dorfloader is not particularly polished. If things break a little, they'll probably break a lot. Feel free to file issues, they'll motivate me to fix or eventually rewrite it.
+* If loading certain resources that do not properly expose their total size, the `loaded` property may exceed the `total` property. Keep this in mind if using this to provide a progress bar, and if it really bothers you, fix your damn server and/or mirror the resources.
---
+++ dorf.js
@@ -0,0 +1,122 @@
+(function(uri) {
+
+var constructor = function() {
+	this.eventTarget = document.createDocumentFragment();
+	this.total = 0;
+	this.start = Date.now();
+	this.totalTime = 0;
+	this.loaded = 0;
+	this.loadedItems = 0;
+	this.elements = [];
+	this.last = 0;
+	this.finals = [];
+	this.progress = function(e) {
+		if(!e.target.loaderinitialized)
+		{
+			Dorf.total += e.total;
+			e.target.loaderinitialized = "true";
+			e.target.loaderlast = 0;
+		}
+		var since = e.timeStamp - Dorf.last,
+			lastloaded = Number(e.target.loaderlast);
+		Dorf.loaded += (e.loaded - lastloaded);
+		e.target.loaderlast = e.loaded;
+		if(since > 100) // Only emit progress events every 100ms
+		{
+			Dorf.last = e.timeStamp;
+			Dorf.dispatchEvent(new ProgressEvent("progress", {"lengthComputable": true, "loaded": Dorf.loaded, "total": Dorf.total}));
+		}
+	};
+	this.load = function(e) {
+		Dorf.loadedItems++;
+		var cfg = e.target.config,
+			el = document.createElement(cfg[0]);
+
+		if(cfg[1])
+		{
+			el.src = URL.createObjectURL(e.target.response);
+		} else
+		{
+			el.innerHTML = e.target.responseText;
+		}
+		Dorf.resources[e.target.res] = el;
+		if(cfg[2] instanceof HTMLElement) // Only a Sith deals in absolutes! Limited instanceof usage is fine.
+		{
+			Dorf.finals.push(el);
+			el.targetElement = cfg[2];
+		}
+
+		if(Dorf.loadedItems >= Dorf.elements.length)
+		{
+			// Process any DOM-insertables
+			for(var i = 0; i < Dorf.finals.length; i++)
+			{
+				Dorf.finals[i].targetElement.appendChild(Dorf.finals[i]);
+				delete Dorf.finals[i].targetElement;
+			}
+			delete Dorf.finals;
+			Dorf.totalTime = Date.now() - Dorf.start;
+			Dorf.dispatchEvent(new Event("load"));
+		}
+	};
+	this.associations = {
+		// Tag to use, is it binary, element to attach to once loaded
+		"js": ["script", false, document.head],
+		"jpg": ["img", true, null],
+		"png": ["img", true, null],
+		"ogg": ["audio", true, null],
+		"mp3": ["audio", true, null],
+		"webm": ["video", true, null],
+		"mp4": ["video", true, null],
+		"css": ["style", false, document.head]
+	};
+	this.resources = {};
+};
+constructor.prototype.addEventListener = function addEventListener(type, callback, capture, untrusted) { return this.eventTarget.addEventListener(type, callback, capture, untrusted); };
+constructor.prototype.removeEventListener = function removeEventListener(type, callback, capture) { return this.eventTarget.removeEventListener(type, callback, capture); };
+constructor.prototype.dispatchEvent = function dispatchEvent(event) { return this.eventTarget.dispatchEvent(event); };
+constructor.prototype.get = function(res)
+{
+	return this.resources.hasOwnProperty(res)? this.resources[res] : null;
+};
+
+Dorf = new constructor();
+
+var resXHR = new XMLHttpRequest();
+resXHR.open("GET", uri);
+resXHR.addEventListener("load", function() {
+	var list = JSON.parse(resXHR.responseText),
+		prefix = uri.match(/(.*\/)[^\/]+$/);
+	if(!prefix)
+	{
+		prefix = "";
+	} else
+	{
+		prefix = prefix[1];
+	}
+	for(var i = 0; i < list.length; i++)
+	{
+		var item = list[i],
+			ext = item.match(/\.(\w+)$/)[1];
+
+		if(Dorf.associations.hasOwnProperty(ext))
+		{
+			var cfg = Dorf.associations[ext],
+				xhr = new XMLHttpRequest();
+			Dorf.elements.push(xhr);
+			xhr.res = item;
+			xhr.config = cfg;
+			if(cfg[1])
+			{
+				xhr.responseType = "blob";
+			}
+			xhr.addEventListener("progress", Dorf.progress);
+			xhr.addEventListener("load", Dorf.load);
+			xhr.open("GET", prefix + item);
+			xhr.send("");
+		}
+	}
+});
+resXHR.send("");
+
+})(document.currentScript.dataset.manifest || "manifest.json");
